493f425d6cd1809f5a9e291834bf341c75d0b241
omarpoblete1992-sudo <omar.poblete1992@gmail.com>
2026-02-20 16:54:21 -0300

    Update schemas.ts

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -5,8 +5,8 @@ export const RegisterSchema = z.object({
     password: z.string().min(8, { message: "La contraseña debe tener al menos 8 caracteres" }),
     nickname: z.string().min(3, { message: "El apodo debe tener al menos 3 caracteres" }),
     birthDate: z.string().refine((date) => new Date(date).toString() !== 'Invalid Date', { message: "Fecha inválida" }),
-    gender: z.enum(["male", "female", "non-binary", "other"], { required_error: "Selecciona tu género" }),
-    interestedIn: z.enum(["male", "female", "everyone"], { required_error: "Selecciona tu interés" }),
+    gender: z.enum(["male", "female", "non-binary", "other"], { message: "Selecciona tu género" }),
+    interestedIn: z.enum(["male", "female", "everyone"], { message: "Selecciona tu interés" }),
 });
 
 export type RegisterSchemaType = z.infer<typeof RegisterSchema>;
