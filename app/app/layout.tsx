11fabbffefe91a337537bbd51dedbd863f644090
omarpoblete1992-sudo <omar.poblete1992@gmail.com>
2026-02-23 12:10:25 -0300

    Update layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
 "use client"
+export const dynamic = 'force-dynamic';
 
 import { Sidebar } from "@/components/layout/sidebar"
 import { ProtectedRoute } from "@/components/auth/protected-route"
