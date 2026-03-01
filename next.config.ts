1ac119cc69a2bcfc07f9f90e57bcd8a53a5bae42
omarpoblete1992-sudo <Omarpoblete1992@gmail.com>
2026-02-28 23:57:32 -0300

    fix: exclude functions directory from Next.js build and ESLint

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
+  eslint: {
+    dirs: ['app', 'components', 'lib', 'hooks', 'types'],
+  },
 };
 
 export default nextConfig;
